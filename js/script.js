52cd8338044d64c81257b5e4e9a402695db5c2de
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-23 15:31:34 +0200

    add js data

--- js/script.js
+++ js/script.js
@@ -18,9 +18,31 @@
 const app = new Vue ({
     el: "#root",
     data: {
-
+        todos: [
+            {
+                text: "Fare la spesa",
+                isDone: false,
+            },
+            {
+                text: "Fare il bucato",
+                isDone: true,
+            },
+            {
+                text: "Andare in banca",
+                isDone: false,
+            },
+            {
+                text: "Pagare le bollette",
+                isDone: false,
+            },
+            {
+                text: "Preparare la cena",
+                isDone: true,
+            }
+        ],
+        newTodo: "",
     },
     methods: {
-
+        
     },
 });
